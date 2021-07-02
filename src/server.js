d76c7e6e88506ccb4a798c90edf1ecc74a8623f6
Caio Jardim <caiojardinho@gmail.com>
2021-07-02 18:17:39 -0300

    created index page

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const routes = require("./routes");
 
 server.set("view engine", "ejs");
 
+server.use(express.static("public"));
+
 server.use(routes);
 
 server.listen(3000, () => console.log("rodando"));
