8ad19de7ac444fd6ac3a797ba64c17f5ecad8ec0
Caio Jardim <caiojardinho@gmail.com>
2021-07-19 13:01:58 -0300

    Modal html and css created

---
+++ public/scripts/modal.js
@@ -0,0 +1,10 @@
+function showModal(product) {
+	console.log("Salve");
+	const html = `
+    <div>
+      <h1>Hello Word</h1>
+    </div>
+  `;
+	const modal = document.querySelector(".modal-container");
+	modal.innerHTML = html;
+}
